184175f10534acb76f34ad86d1867c6024db303e
Babu Matlori <babumatlori@gmail.com>
2026-08-04 19:31:03 +0530

    solved the pattern 01 Sqaure fill Pattern

---
+++ src/Square.java
@@ -0,0 +1,18 @@
+public class Square{
+    public static void pattern1(int n) {
+        int i;
+        int j;
+
+        for( i = 0; i < n; i++) {
+            for( j =0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        pattern1(n);
+    }
+}
