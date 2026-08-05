1bfcbc77515addcd68c62b445bd82e4fd1bfc904
Babu Matlori <babumatlori@gmail.com>
2026-08-05 17:40:08 +0530

    SOlved the Triangle Pyramid Pattern 07

---
+++ src/Pattern07.java
@@ -0,0 +1,34 @@
+// Triangle Star Pattern
+
+import java.util.Scanner;
+
+public class Pattern07 {
+
+    static void TrianglePattern(int n) {
+        int i;
+        int j;
+
+        for( i = 0; i < n; i++) {
+            for(j = n- i; j>1; j--  ) {
+                System.out.print(" ");
+            }
+                for( j = 0; j<=i; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n0:");
+
+        if(sc.hasNextInt()) {
+            int n= sc.nextInt();
+            TrianglePattern(n);
+        }
+
+        sc.close();
+    }
+
+}
